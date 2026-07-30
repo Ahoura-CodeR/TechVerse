f5d62a191ce5894df1471c21741342cff794334a
Ahoura <AhouraCodeR@gmail.com>
2026-07-31 01:26:32 +0330

    add differ

---
+++ ThchVerse/js/component/product/product.js
@@ -0,0 +1,62 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <link rel="stylesheet" href="/js/component/product/product.css">
+  <div class="product-container">
+  </div>
+  
+  `
+
+
+let getProducts = async () => {
+  try {
+    const res = await fetch('https://dummyjson.com/products/search?q=phone')
+    const data = await res.json()
+    if (!res.ok) throw new Error('خطا در دریافت اطلاعات ')
+    return data.products || []
+    
+  } catch (error) {
+    console.log('your error ==> ' + error)
+    return []
+  }
+} 
+
+class isProducts extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+  async connectedCallback() {
+    let fragment = document.createDocumentFragment()
+    const product = await getProducts()
+    const productContainer = this.shadowRoot.querySelector('.product-container')
+    console.log(product)
+    product.forEach((item) => {
+
+      const boxProduct = document.createElement('div')
+
+      boxProduct.innerHTML = `
+        <div class="box-product">
+            <svg class="svg-fav" width="26" height="23" viewBox="0 0 26 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.88776 12.7678L12.2689 21.5804C12.5933 21.8851 12.7555 22.0375 12.9535 22.0375C13.1515 22.0375 13.3138 21.8851 13.6382 21.5804L23.0193 12.7678C25.6275 10.3177 25.9443 6.28568 23.7506 3.45831L23.3381 2.92667C20.7139 -0.455676 15.4464 0.11157 13.6024 3.97509C13.342 4.52083 12.5651 4.52083 12.3046 3.97509C10.4607 0.11157 5.19316 -0.455676 2.56893 2.92667L2.15645 3.45831C-0.0372038 6.28568 0.279528 10.3177 2.88776 12.7678Z" stroke="#919191" stroke-opacity="0.77" stroke-width="1.4"/>
+            </svg>
+            <img src="${item.images[0]}">
+            <p class="p-proucts">
+                ${item.description} 
+            </p>
+            <p class="price-products">
+                $${item.price}
+            </p>
+            <button class="button-buy">
+                Buy Now
+            </button>
+        </div>
+        ` 
+      fragment.appendChild(boxProduct)
+      
+    })
+    productContainer.appendChild(fragment)
+  }
+}
+
+export {isProducts, getProducts}
--- ThchVerse/js/home.js
+++ ThchVerse/js/home.js
@@ -1,4 +1,8 @@
 import { isCategory } from "./component/category/category.js";
 import { isFooter } from "./component/footer/footer.js";
+import { isProducts, getProducts } from "./component/product/product.js";
 window.customElements.define("category-component", isCategory)
 window.customElements.define("footer-site", isFooter)
+window.customElements.define("product-data", isProducts)
+
+getProducts ()
