ef4db4b18b92f8b67e8d2ea2523aa1f50a41c233
Ahoura <AhouraCodeR@gmail.com>
2026-09-16 22:30:34 +0330

    edit location file

--- js/products-detail.js
+++ js/products-detail.js
@@ -1,5 +1,5 @@
 import { productsDetail } from "./component/products-detail/detail.js";
-import { isFooter } from "..component/footer/footer.js";
+import { isFooter } from "./component/footer/footer.js";
 
 window.customElements.define("products-detail", productsDetail);
 window.customElements.define("footer-site", isFooter);
@@ -7,10 +7,10 @@ window.customElements.define("footer-site", isFooter);
 const animationLoad = document.querySelector(".container");
 const pageLoader = document.querySelector(".page-loader");
 
-// animationLoad.style.display = "none";
+animationLoad.style.display = "none";
 
-// window.addEventListener("load", (event) => {
-//   animationLoad.style.display = "";
-//   pageLoader.style.display = "none";
-// });
+window.addEventListener("load", (event) => {
+  animationLoad.style.display = "";
+  pageLoader.style.display = "none";
+});
 pageLoader.style.display = 'none'
